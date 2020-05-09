186511810eb0703df70ab5cdd4f63792629eda03
Junki Yamagata <redmotorcycleapps@gmail.com>
2020-05-09 17:25:48 +0900

    PWA化した時のアプリ名定義

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {
 "favicon.png": "4a1a4c74fd396f01f7f19b0217ee3034",
 "icons/Icon-192.png": "b70fb9f3e622fd81fe09a2cd7f478452",
 "icons/Icon-512.png": "05c1832baf6e9cd6b2bf24ccfced0881",
-"manifest.json": "d9ea0a7eb5a46bf79751dec35d582549",
+"manifest.json": "0be05307b4871fa48df4bf146c56ee9c",
 "assets/LICENSE": "638481329f190f3533ef04c102487ba3",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
